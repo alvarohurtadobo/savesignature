5c2cbce535991c477d301e399461f4f2f1be5899
Alvaro Ruben Hurtado Maldonado <a.hurtado.bo@gmail.com>
2019-06-28 00:28:01 -0400

    Added first files

---
+++ build/frontend_server.d
@@ -0,0 +1,1 @@
+frontend_server.d: /home/alvaro/programs/flutter/bin/cache/artifacts/engine/linux-x64/frontend_server.dart.snapshot
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'signaturewidget.dart';
+
+
+const directoryName = 'pest';
+
+void main() {
+  runApp(
+      MaterialApp(
+        home: SignWidget(),
+        debugShowCheckedModeBanner: false,
+      )
+  );
+}
